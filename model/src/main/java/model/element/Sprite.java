c53438a800789c8eb28150b91be7441fd4b8337e
AdrienKawak <33150132+AdrienKawak@users.noreply.github.com>
2019-05-22 17:11:50 +0200

    maj2

---
+++ model/src/main/java/model/element/Sprite.java
@@ -0,0 +1,17 @@
+package model.element;
+
+import java.awt.*;
+
+public class Sprite {
+    private Image image;
+    private String imageName;
+    private char imageWindows;
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}

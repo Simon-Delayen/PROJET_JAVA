9cdc9ef03748d0600f5802ec2eefbe0ea5a8196f
AdrienKawak <33150132+AdrienKawak@users.noreply.github.com>
2019-05-23 08:45:00 +0200

    .

---
+++ model/src/main/java/model/element/fix/Wall.java
@@ -0,0 +1,8 @@
+package model.element.fix;
+
+import contract.model.ISprite;
+import model.element.Sprite;
+
+public class Wall {
+    private static final ISprite SPRITE = new Sprite('O', "wall.png");
+}

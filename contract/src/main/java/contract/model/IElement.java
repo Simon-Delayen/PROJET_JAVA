28daf441bccd3da64ee60a6a987f81639c58884c
AdrienKawak <33150132+AdrienKawak@users.noreply.github.com>
2019-05-24 09:38:59 +0200

    maj

--- contract/src/main/java/contract/model/IElement.java
+++ contract/src/main/java/contract/model/IElement.java
@@ -1,6 +1,7 @@
 package contract.model;
-import showboard.ISquare;
 
-public interface IElement extends ISquare{
+import fr.exia.showboard.ISquare;
+
+public interface IElement extends ISquare {
     ISprite getSprite();
 }

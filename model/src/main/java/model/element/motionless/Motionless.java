8c44ef575f8e6b88a6d99b64a089d57f90dee06f
Boostien <Boostien@users.noreply.github.com>
2019-05-27 10:11:35 +0200

    interface again

--- model/src/main/java/model/element/motionless/Motionless.java
+++ model/src/main/java/model/element/motionless/Motionless.java
@@ -2,4 +2,25 @@ package model.element.motionless;
 
 public class Motionless {
 
+		public int x;
+		public int y;
+		
+		public int getx() {
+			return this.x;
+		}
+		
+		public void setx(int x) {
+			this.x = x;
+		}
+		
+		public int gety() { 
+			return this.y;
+		}
+		
+		public void sety(int y) {
+			this.y = y;
+		}
+
+	}
+
 }
